c5b42cbdfbf9ad71b81f4f26e372ac5a38b353db
wiktuar <wiktuar@yandex.ru>
2023-02-09 20:16:29 +0300

    Done homework Task 9

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-import controller as c
-c.start()
+from telegram import Bot, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+bot = Bot(token= "5948400891:AAEUagQYYa7sLh8vj09J7O7TaCRhVv0ZVWQ")
+updater = Updater(token= "5948400891:AAEUagQYYa7sLh8vj09J7O7TaCRhVv0ZVWQ")
+dispatcher = updater.dispatcher
+
+def correct(update, context):
+    text = update.message.text
+    list = text.split()
+
+    for l in list:
+        if("абв" in l): list.remove(l)
+
+    resultStr = ""
+    for l in list:
+        resultStr += l + " "
+
+    context.bot.send_message(update.effective_chat.id, resultStr)
+
+message_handler = MessageHandler(Filters.command, correct)
+
+dispatcher.add_handler(message_handler)
+
+updater.start_polling()
+updater.idle()
+
+
